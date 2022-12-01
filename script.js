6dadf841608c88115ebb7514873307b270fb40ef
hyper_puncher <39203616+hyperpuncher@users.noreply.github.com>
2022-12-01 12:57:57 +0300

    Add math functions

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-const add = function(n1, n2) {
-  return n1 + n2;
-};
+const add = (n1, n2) => n1 + n2;
 
-const subtract = function(n1, n2) {
-  return n1 - n2;
-};
+const subtract = (n1, n2) => n1 - n2;
 
-const sum = function(array, sum = 0) {
-	for (n in array) sum += array[n];
-  return sum;
-};
+const multiply = (n1, n2) => n1 * n2;
 
-const multiply = function(array, sum = 1) {
-  for (n in array) sum *= array[n];
-  return sum;
-};
+const divide = (n1, n2) => n1 / n2;
 
-const power = function(n1, n2) {
-	return n1 ** n2;
-};
-
-const factorial = function(n, sum = 1) {
-	for (n; n > 1; n--) sum *= n;
-  return sum;
-};
+const operate = (func, n1, n2) => func(n1, n2);
