7d8c9e974d4c7c12f375d7483aa99bd11708cfe1
hyper_puncher <39203616+hyperpuncher@users.noreply.github.com>
2022-12-02 13:50:43 +0300

    upd

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const equalButton = document.querySelector('#operate');
 const screen = document.querySelector('.screen');
+
 const add = (n1, n2) => n1 + n2;
 const subtract = (n1, n2) => n1 - n2;
 const multiply = (n1, n2) => n1 * n2;
 const divide = (n1, n2) => n1 / n2;
-const operate = (func, n1, n2) => func(n1, n2);
+const percentage = (n1, n2) => n1 * (n2 / 100);
+const round = (n) => Math.round(n * 10000) / 10000;
+const operate = (operator, n1, n2) => {
+  if (operator === 'add') return round(add(n1, n2));
+  else if (operator === 'subtract') return round(subtract(n1, n2));
+  else if (operator === 'multiply') return round(multiply(n1, n2));
+  else if (operator === 'divide') return round(divide(n1, n2));
+  else if (operator === 'percentage') return round(percentage(n1, n2));
+};
+
 let displayValue = '';
 let n1 = 0;
 let n2 = 0;
@@ -14,31 +24,46 @@ let operator = '';
 
 
 numberButtons.forEach(btn => btn.addEventListener('click', (e) => {
-  screenContent(e);
+  if (typeof displayValue !== 'number' && displayValue.includes('.') && e.target.id === '.') return 0;
+  else screenContent(e);
 }));
 
 operatorButtons.forEach(btn => btn.addEventListener('click', (e) => {
-  n1 = parseFloat(displayValue);
-  operator = e.target.id;
+  if (e.target.id === 'reset') {
+    n1 = 0;
+    n2 = 0;
+    operator = '';
+  } else if (displayValue === operator) {
+    operator = e.target.id;
+  } else if (operator) {
+    n2 = parseFloat(displayValue)
+    n1 = operate(operator, n1, n2);
+    operator = e.target.id;
+  } else {
+    n1 = parseFloat(displayValue);
+    operator = e.target.id;
+  }
   displayValue = '';
   screenContent(e);
 }));
 
 equalButton.addEventListener('click', () => {
-  n2 = parseFloat(displayValue);
-  console.log(n1);
-  console.log(n2);
-  console.log(operator);
-  let func = scope[operator];
-  console.log(func);
-  console.log(typeof func);
-  displayValue = operate(func, n1, n2);
-  operator = '';
+  if (operator) {
+    n2 = parseFloat(displayValue);
+    displayValue = operate(operator, n1, n2);
+    if (displayValue === Infinity) displayValue = 'R U STUPID?'
+    screen.innerText = displayValue;
+    n1 = 0;
+    n2 = 0;
+    operator = '';
+  }
 });
 
-
 function screenContent(btn) {
-  if (displayValue == operator) displayValue = '';
+  if (displayValue == operator 
+    || displayValue == 'reset' 
+    || displayValue == 'R U STUPID?' 
+    || typeof displayValue === 'number') displayValue = '';
   displayValue += btn.target.id;
   screen.innerText = displayValue;
 }
