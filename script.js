23fa29eea012c96c021b8e471a831c922a42e00e
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2022-12-03 16:29:02 +0300

    upd

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const equalButton = document.querySelector('#operate');
+const backspaceButton = document.querySelector('#backspace');
+const deleteButton = document.querySelector('#delete');
 const screen = document.querySelector('.screen');
 
 const add = (n1, n2) => n1 + n2;
@@ -10,68 +12,64 @@ const divide = (n1, n2) => n1 / n2;
 const percentage = (n1, n2) => n1 * (n2 / 100);
 const round = (n) => Math.round(n * 10000) / 10000;
 const operate = (operator, n1, n2) => {
-  if (operator === 'add') return round(add(n1, n2));
-  else if (operator === 'subtract') return round(subtract(n1, n2));
-  else if (operator === 'multiply') return round(multiply(n1, n2));
-  else if (operator === 'divide') return round(divide(n1, n2));
-  else if (operator === 'percentage') return round(percentage(n1, n2));
+  if (operator === '+') return round(add(n1, n2));
+  else if (operator === '-') return round(subtract(n1, n2));
+  else if (operator === '*') return round(multiply(n1, n2));
+  else if (operator === '/') return round(divide(n1, n2));
+  else if (operator === '%') return round(percentage(n1, n2));
 };
 
 let displayValue = '';
 let n1 = 0;
 let n2 = 0;
 let operator = '';
-const operatorSymbols = ['=', '*', '/', '-', '+', '%']
-
+const operatorSymbols = ['*', '/', '-', '+', '%']
 
 window.addEventListener('keydown', (e) => {
-  console.log(e.target.id);
-  if (parseInt(e.key) >= 0 && parseInt(e.key) <= 9) numbers(e);
-
+  if (parseInt(e.key) >= 0 && parseInt(e.key) <= 9) numbers(e.key);
+  else if (e.key === 'Backspace') backspace();
+  else if (operatorSymbols.includes(e.key)) operators(e.key);
+  else if (e.key === '=') equals()
 })
 
-numberButtons.forEach(btn => btn.addEventListener('click', (e) => numbers(e)));
-
-operatorButtons.forEach(btn => btn.addEventListener('click', (e) => operators(e)));
-
+numberButtons.forEach(btn => btn.addEventListener('click', (e) => numbers(e.target.id)));
+operatorButtons.forEach(btn => btn.addEventListener('click', (e) => operators(e.target.id)));
 equalButton.addEventListener('click', () => equals());
+backspaceButton.addEventListener('click', () => backspace());
+deleteButton.addEventListener('click', () => deleteNumber());
 
 
 
 function screenContent(btn) {
-  if (displayValue == operator 
-    || displayValue == 'reset' 
-    || displayValue == 'R U STUPID?' 
+  if (displayValue == operator
+    || displayValue == 'reset'
+    || displayValue == 'R U STUPID?'
     || typeof displayValue === 'number') displayValue = '';
-  if (btn.target.id) {
-    displayValue += btn.target.id;
-  } else {
-    displayValue += btn.key;
-  }
+  displayValue += btn;
   screen.innerText = displayValue;
 }
 
 function numbers(btn) {
-  if (typeof displayValue !== 'number' 
-    && displayValue.includes('.') 
+  if (typeof displayValue !== 'number'
+    && displayValue.includes('.')
     && btn.target.id === '.') return 0;
   else screenContent(btn);
 }
 
 function operators(btn) {
-  if (btn.target.id === 'reset') {
+  if (btn === 'reset') {
     n1 = 0;
     n2 = 0;
     operator = '';
   } else if (displayValue === operator) {
-    operator = btn.target.id;
+    operator = btn;
   } else if (operator) {
     n2 = parseFloat(displayValue)
     n1 = operate(operator, n1, n2);
-    operator = btn.target.id;
+    operator = btn;
   } else {
     n1 = parseFloat(displayValue);
-    operator = btn.target.id;
+    operator = btn;
   }
   displayValue = '';
   screenContent(btn);
@@ -87,4 +85,14 @@ function equals() {
     n2 = 0;
     operator = '';
   }
+}
+
+function backspace() {
+  displayValue = displayValue.slice(0, -1);
+  screen.innerText = displayValue;
+}
+
+function deleteNumber() {
+  displayValue = '';
+  screen.innerText = displayValue;
 }
